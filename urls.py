66cb4d0916175fa5c33e7c8454c87242bdd871a6
gagan31799 <35043537+gagan31799@users.noreply.github.com>
2019-01-13 11:28:27 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 11 13:21:19 2019
+
+@author: Garg
+"""
+
+from django.urls import path
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
